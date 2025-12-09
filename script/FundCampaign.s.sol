57ed15fdf8c78ff42a2172416226f87a3bc4e9c4
aurel-fr <105201452+aurel-fr@users.noreply.github.com>
2025-12-08 18:07:03 -0800

    refactor: script

--- script/FundCampaign.s.sol
+++ script/FundCampaign.s.sol
@@ -7,19 +7,16 @@ import {IERC20} from "../src/interfaces/IERC20.sol";
 import {WorldCampaignManager} from "../src/WorldCampaignManager.sol";
 
 contract FundCampaignScript is Script {
-    function setUp() public {}
+    // Deployed WorldCampaignManager contract address
+    address campaignManagerAddress = 0xcf2Ab38031cFe6e5A3b7f17b0f6406F8ecF43f57;
+    // Token address to use for rewards
+    address tokenAddress = 0x2cFc85d8E48F8EAB294be644d9E25C3030863003; // WLD
+    // Campaign parameters
+    uint256 campaignId = 3;
 
     function run() public {
-        // Deployed WorldCampaignManager contract address
-        address campaignManagerAddress = 0xcf2Ab38031cFe6e5A3b7f17b0f6406F8ecF43f57;
-
-        // Token address to use for rewards
-        address tokenAddress = 0x2cFc85d8E48F8EAB294be644d9E25C3030863003; // WLD
-
-        // Campaign parameters
-        uint256 campaignId = 3;
+        // TODO set to correct amount
         uint256 amount = 0.01 ether;
-        // ===============================================
 
         WorldCampaignManager campaignManager = WorldCampaignManager(
             campaignManagerAddress
